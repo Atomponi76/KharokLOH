773d0b0244dd155ffc919db74e7a59c5170e33b0
Atomponi76 <atomponi76@gmail.com>
2022-09-24 12:58:49 +0300

    Inita

---
+++ Manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Manager : Worker
+    {
+        public int ClientCount { get; set; }
+
+        public Manager(string name, int age, double salary,int clientCount) 
+            : base(name,age,salary)
+        {
+            Console.WriteLine("manager full constructor");
+            ClientCount = clientCount;
+        }
+        public Manager() : base()
+        {
+            Console.WriteLine("Manager defoult constructer");
+        }
+
+        public override double GetResultSalary()
+        {
+            return Salary * ClientCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp10;
+
+Manager maneger = new Manager("Anna ", 22, 15000, 9);
+
+Console.WriteLine(maneger.GetResultSalary());
